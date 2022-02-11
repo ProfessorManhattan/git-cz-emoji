2caf0ef0eb6e74bca91e34ec7d399ee81cb6a8a9
Brian Zalewski <brian@megabyte.space>
2022-02-11 09:48:30 -0500

    🐛 fix(emojis): using same width emojis now

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 export const types = {
   build: {
     description: 'Changes that affect the build system or external dependencies (example scopes: gulp, pnpm)',
-    emoji: '🏗 ',
+    emoji: '🏗️',
     title: 'Builds'
   },
   chore: {
@@ -36,7 +36,7 @@ export const types = {
   },
   refactor: {
     description: 'A code change that neither fixes a bug nor adds a feature',
-    emoji: '♻️ ',
+    emoji: '🧐',
     title: 'Code Refactoring'
   },
   revert: {
